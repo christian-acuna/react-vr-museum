c31e009043bc4357e058afb9d21cfdd58c396789
parinaz77 <parinaz77@gmail.com>
2016-12-21 12:22:00 -0800

    add list of collections to profile page

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -4,7 +4,24 @@ import ListItem from 'material-ui/List/ListItem';
 import Avatar from 'material-ui/Avatar';
 import FileFolder from 'material-ui/svg-icons/file/folder';
 import List from 'material-ui/List/List';
+import {GridList} from 'material-ui/GridList';
+import Subheader from 'material-ui/Subheader';
+import CollectionCard from './CollectionCard';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 900,
+    height: '100%',
+    overflowY: 'auto',
+    color: 'gray'
+  },
+};
 
 class UserProfile extends React.Component {
 
@@ -16,6 +33,20 @@ class UserProfile extends React.Component {
 		console.log("hi")
 		this.props.getUserCollections()
 	}
+
+	renderDefault() {
+    return this.props.currentUser.user.collections.map((collection, i) => (
+      <CollectionCard
+      	style={styles.card}
+        key={i}
+        collectionId={collection.id}
+        title={collection.title}
+        image={collection.primary_object.image_url}
+        user={collection.user}
+        >
+      </CollectionCard>
+    ))
+  }
 
   render() {
     return(
@@ -33,18 +64,16 @@ class UserProfile extends React.Component {
     {localStorage.username}
   
     </ListItem>
-
-    <h3>User Collections</h3>
-    <ul>
-    	{
-    		this.props.currentUser.user.collections.map((collection,i) => {
-    			return(
-    		<li>{collection.title}</li>
-    		)
-    	})
-    }
-    </ul>
-
+	    	<div style={styles.root}>
+		    	<GridList
+		          cellHeight={400}
+		          cols={3.1}
+		          style={styles.gridList}
+		        >
+		          <Subheader>User Collections</Subheader>
+		          {this.renderDefault()}
+		       </GridList>
+		    </div>
       </div>
     );
   }
@@ -52,3 +81,15 @@ class UserProfile extends React.Component {
 }
 
 export default UserProfile;
+
+    // <ul>
+    // 	{
+    // 		this.props.currentUser.user.collections.map((collection,i) => {
+    // 			return(
+    // 				<div>
+    // 		<li>{collection.title}</li>
+    // 		<img src={collection.primary_object.image_url} /></div>
+    // 		)
+    // 	})
+    // }
+    // </ul>

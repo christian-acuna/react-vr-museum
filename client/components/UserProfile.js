b8cd5a58aa4c6cf21bcaf8e3948df111cacd27ae
parinaz77 <parinaz77@gmail.com>
2016-12-21 18:00:17 -0800

    create new collection on user profile page

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -8,6 +8,10 @@ import {GridList} from 'material-ui/GridList';
 import Subheader from 'material-ui/Subheader';
 import CollectionCard from './CollectionCard';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+const username = localStorage.username
 
 const styles = {
   root: {
@@ -24,17 +28,43 @@ const styles = {
   tileFont:{
     fontFamily: 'Roboto',
     padding: '40px',
-  }
+  },
+  button:{
+    display:'block',
+    margin: '0 auto',
+    width: '40%',
+  },
+  box: {
+  	color: 'gray',
+  	width: '50%',
+     margin: '0 auto',
+  	// border: 'solid',
+  	// margin: '0 auto',
+  	// display: 'block',
+  	// width: '50%',
+  	// height: '50%',
+
+  	// verticalAlign: 'middle',
+  },
+  buttonStyle: {
+    margin: 12,
+  },
 };
 
 class UserProfile extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.state={
+			showCreateCollection:false
+		}
+		this.showForm = this.showForm.bind(this)
+		this.hideForm = this.hideForm.bind(this)
+		this.handleChange = this.handleChange.bind(this)
 	}
 
 	componentWillMount() {
-		console.log("hi")
 		this.props.getUserCollections()
 	}
 
@@ -52,32 +82,70 @@ class UserProfile extends React.Component {
     ))
   }
 
+  showForm(){
+  	this.setState({
+  		showCreateCollection: true
+  	})
+  }
+  
+  handleChange() {
+  	const title = this.refs.title.input.value;
+  	const description = this.refs.description.input.value;
+  	console.log(title, description)
+  	this.hideForm();
+  	console.log(this)
+  	this.props.addNewCollection(title, 1098);
+  }
+
+  hideForm(){
+  		this.setState({
+  		showCreateCollection: false
+  	})
+  }
+
   render() {
     return(
     	<div style={styles.tileFont}>
-	      <h2 > User Profile</h2>
-	    	<ListItem
-      disabled={true}
-      leftAvatar={
-        <Avatar
-          icon={<FontIcon className="muidocs-icon-communication-voicemail" />}
-          backgroundColor={'#D9D4CF'}
-        />
-      }
-    >
-    {localStorage.username}
-  
-    </ListItem>
-	    	<div style={styles.root}>
-		    	<GridList
-		          cellHeight={400}
-		          cols={3.1}
-		          style={styles.gridList}
-		        >
-		          <Subheader>User Collections</Subheader>
-		          {this.renderDefault()}
-		       </GridList>
-		    </div>
+	      
+	  	    <RaisedButton 
+	  	    		label={`Create a new collection ${username}`} 
+	  	    		primary={true} style={styles.button} 
+	  	    		onClick={this.showForm}
+
+	  	    		/>
+	  	    <br />
+	  	    <br />
+
+	  	    {
+	  	    	this.state.showCreateCollection ? 
+	  	    	<div style={{margin: '0 auto', width: '17%', textAlign: 'center'}}>
+	            <TextField ref="title"
+	              floatingLabelText="Title"/>
+	            <br />
+
+	            <TextField
+	              floatingLabelText="Description"
+	              ref="description"/>
+	              <br />
+	              <RaisedButton onClick={this.handleChange} label="Submit"
+	                secondary={true}
+	                style={styles.buttonStyle}
+	              />
+          	</div> 
+
+
+	  	    	: 
+				    	<div style={styles.root}>
+					    	<GridList
+					          cellHeight={400}
+					          cols={3.1}
+					          style={styles.gridList}
+					        >
+					          <Subheader>{`${username}'s Collections`}</Subheader>
+					          {this.renderDefault()}
+					       </GridList>
+					    </div>
+	  	    }
       </div>
     );
   }
@@ -85,15 +153,3 @@ class UserProfile extends React.Component {
 }
 
 export default UserProfile;
-
-    // <ul>
-    // 	{
-    // 		this.props.currentUser.user.collections.map((collection,i) => {
-    // 			return(
-    // 				<div>
-    // 		<li>{collection.title}</li>
-    // 		<img src={collection.primary_object.image_url} /></div>
-    // 		)
-    // 	})
-    // }
-    // </ul>
